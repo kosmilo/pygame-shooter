b28a192658327fdd81d8ce7c929e5c8e924df8f4
noora ha <noora.halla.aho@gmail.com>
2023-12-07 08:56:22 +0200

    clean up hand tracking code
    - add check to make sure the cursor doesn't go out of boundaries
    - turn functions into static methods
    - improve code readability

--- hand_tracking.py
+++ hand_tracking.py
@@ -12,24 +12,6 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 mp_draw = mp.solutions.drawing_utils
 
 
-def is_hand_visible(result: GestureRecognizerResult):
-    if len(result.hand_landmarks) > 0:
-        return True
-    else:
-        return False
-
-
-def get_hand_location(result: GestureRecognizerResult):
-    hand_landmarks = result.hand_landmarks[0]
-    hand_location = hand_landmarks[0].x, hand_landmarks[0].y
-    return hand_location
-
-
-def get_hand_gesture_name(result: GestureRecognizerResult):
-    hand_gesture = result.gestures[0][0].category_name
-    return hand_gesture
-
-
 class HandTracking:
     def __init__(self, game):
         self.game = game
@@ -38,7 +20,7 @@ class HandTracking:
         self.options = GestureRecognizerOptions(
             base_options=BaseOptions(model_asset_path=self.model_path),
             running_mode=VisionRunningMode.LIVE_STREAM,
-            result_callback=self.update_hand_info
+            result_callback=self.update_hand_info  # this controls what function to call for the output
         )
         self.recognizer = vision.GestureRecognizer.create_from_options(self.options)
 
@@ -46,30 +28,56 @@ class HandTracking:
     def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
         print('timestamp: {}, gesture recognition result: {}'.format(timestamp_ms, result))
 
-    @staticmethod
-    def print_hand_info(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-        if is_hand_visible(result):
-            print('timestamp: {}, gesture: {}, location: {}'.format(
-                timestamp_ms, get_hand_gesture_name(result), get_hand_location(result)))
-
-    def update_hand_info(self, result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-        if is_hand_visible(result):
-            x, y = get_hand_location(result)
+    def print_hand_info(self, result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+        if self.is_hand_visible(result):
+            x, y = self.get_hand_location(result)
             screen_x = WIDTH * (1 - x)
             screen_y = HEIGHT * y
-            pg.mouse.set_pos(screen_x, screen_y)
-            if get_hand_gesture_name(result) == 'Closed_Fist':
-                closed_fist_event = pg.event.Event(CLOSED_FIST)
-                pg.event.post(closed_fist_event)
-            print('hand visible')
+            print('timestamp: {}, gesture: {}, x: {} out of {}, y: {} out of {}'.format(
+                timestamp_ms, self.get_hand_gesture_name(result), screen_x, WIDTH, screen_y, HEIGHT))
         else:
             print('hand not visible')
 
-    def get_pos(self):
-        return self.x, self.y
+    def update_hand_info(self, result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+        if self.is_hand_visible(result):
+            # get hand location, translate it into screen coordinates and set mouse pos
+            # the max/min stuff is so the cursor can't go out of boundaries and break everything
+            x, y = self.get_hand_location(result)
+            screen_x = max(20, min(WIDTH * (1 - x), WIDTH-20))
+            screen_y = max(20, min(HEIGHT * y, HEIGHT-20))
+            pg.mouse.set_pos(screen_x, screen_y)
+
+            # this is for debugging purposes
+            print('timestamp: {}, gesture: {}, x: {} out of {}, y: {} out of {}'.format(
+                timestamp_ms, self.get_hand_gesture_name(result), screen_x, WIDTH, screen_y, HEIGHT))
+
+            # if the gesture is a closed fist, activate event to be used in place of mouse click
+            if self.get_hand_gesture_name(result) == 'Closed_Fist':
+                closed_fist_event = pg.event.Event(CLOSED_FIST)
+                pg.event.post(closed_fist_event)
+        else:
+            print('hand not visible')  # this can be replaced with a warning or some such
+
+    @staticmethod
+    def is_hand_visible(result: GestureRecognizerResult):
+        if len(result.hand_landmarks) > 0:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_hand_location(result: GestureRecognizerResult):
+        hand_landmarks = result.hand_landmarks[0]
+        hand_location = hand_landmarks[0].x, hand_landmarks[0].y
+        return hand_location
+
+    @staticmethod
+    def get_hand_gesture_name(result: GestureRecognizerResult):
+        hand_gesture = result.gestures[0][0].category_name
+        return hand_gesture
 
     def update(self):
-        ret, frame = self.vid.read()
-        latest_frame = frame
-        mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=latest_frame)
-        self.recognizer.recognize_async(mp_frame, pg.time.get_ticks())
+        ret, frame = self.vid.read()  # capture video
+        mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)  # convert frame to mp image
+        self.recognizer.recognize_async(mp_frame, pg.time.get_ticks())  # use gesture recognition on the mp image
+        cv2.imshow('frame', frame)  # display camera feed
